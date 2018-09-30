3964442229bd57efec058c45acd420194dcaa00b
Karol Martyński <43373840+kmartynski@users.noreply.github.com>
2018-09-30 12:09:27 +0200

    Update iodata.py

--- iodata.py
+++ iodata.py
@@ -33,20 +33,14 @@ class InputConverter:
         initime, = struct.unpack('>H', file[48:50])
         expectedMatrixWidth = (datalen - 72)/4
         outputMatrix = np.zeros((2, expectedMatrixWidth))
-        index = 0;
+        index = 0
         print "Processing..."
         for i in range(64, datalen-8, 4):
             e1, e2 = struct.unpack('>HH',file[i:i+4])
             outputMatrix[0, index] = (e1 - self.midAdc)/self.convFactor
             outputMatrix[1, index] = (e2 - self.midAdc)/self.convFactor
             index += 1
-            #with open('S:/Doktorat/Dane/Hugo/'+"201604010000" + ".csv" ,"w") as out_file:
-                #for j in range(len(outputMatrix)):
-                    #out_string = ""
-                    #out_string += str(outputMatrix)
-                    #out_string += "\n"
-                    #out_file.write(out_string)
-
+            
         return DataStruct(outputMatrix,initime,headerString)
 
 
