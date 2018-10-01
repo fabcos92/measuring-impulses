1d5e5ef2a2546baa4d457f5cb81e68fef95afcd4
Karol Martyński <43373840+kmartynski@users.noreply.github.com>
2018-10-01 09:11:52 +0200

    Add files via upload

---
+++ Module.py
@@ -0,0 +1,13 @@
+# Input variables
+h = "Hylaty"
+u = 'Hugo'
+p = 'Patagonia'
+
+Station = input('Enter a station name (Hylaty = h, Hugo = u, Patagonia = p): ')
+Date = raw_input('Enter date time (yyyymmdd): ')
+Date_end = raw_input('Enter end date time (yyyymmdd): ')
+Start_time_hours = int(raw_input('Enter start time (hh): '))
+Start_time_minutes = int(raw_input('Enter start time (mm): '))
+End_time_hours = int(raw_input('Enter end time (hh): '))
+End_time_minutes = int(raw_input('Enter end time (mm): '))
+Save_Print_SavPrin = raw_input('Press s = Save, p = Plot: ')
